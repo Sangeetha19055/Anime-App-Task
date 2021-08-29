aba6bad05618d8a033afe7c370259e5e5a393cc0
Sangeetha <89621488+Sangeetha19057@users.noreply.github.com>
2021-08-29 11:13:43 +0300

    Update scirpt.js

--- scirpt.js
+++ scirpt.js
@@ -29,7 +29,7 @@ function searchAnime(event) {
     // getAnime(tobefindtext);
    
     if(tobefindtext == ""){
-       alert("please!!! Enter the name to find anime");
+       alert("please!!! Enter the Name to find anime");
       }else{
         getAnime(tobefindtext);
       }
