2894be7a85471bacc9373ac72a99c30e4775f885
Sangeetha <89621488+Sangeetha19057@users.noreply.github.com>
2021-08-29 13:04:35 +0300

    Update scirpt.js

--- scirpt.js
+++ scirpt.js
@@ -1,6 +1,5 @@
 const apiurl = "https://api.jikan.moe/v3";
 
-
 //------------------------  Topnav anf from function  ----------------------
 function Topnav_And_Form() {
     //navbar 
@@ -9,15 +8,17 @@ function Topnav_And_Form() {
     h1.textContent = "Anime App";
     h1.setAttribute("class", "title");
     nav.append(h1);
+    const section = document.createElement("section");
+
     const formdiv = document.createElement("div");
     formdiv.setAttribute("class", "conatiner");
     formdiv.innerHTML = `
-    <form>
-    <input type="text" name="search" id="search" placeholder="search">
-    <button type="button" onclick="searchAnime(event)">search</button>
-    </form>
-    `;
-    document.querySelector(".form").append(nav, formdiv);
+     <form>
+        <input type="text" name="search" id="search" placeholder="search">
+        <button type="button" onclick="searchAnime(event)">search</button>
+     </form>`;
+    section.append(formdiv);
+    document.querySelector(".form").append(nav, section);
 }
 Topnav_And_Form();
 
@@ -27,19 +28,19 @@ function searchAnime(event) {
     let inputtext = document.getElementById("search");
     let tobefindtext = inputtext.value;
     // getAnime(tobefindtext);
-   
-    if(tobefindtext == ""){
-       alert("please!!! Enter the Name to find anime");
-      }else{
+
+    if (tobefindtext == "") {
+        window.alert("Please!!! 😀 Enter the name to find Anime");
+    } else {
         getAnime(tobefindtext);
-      }
-     inputtext.value = "";
-      
+    }
+    inputtext.value = "";
 }
+
 //-------------------------- setting  initial API to be loaded ---------------------------
 async function getAnime(find) {
     try {
-        const data = await fetch(`${apiurl}/search/anime?q=${find}&page=1`,{
+        const data = await fetch(`${apiurl}/search/anime?q=${find}&page=1`, {
             method: "GET"
         });
         const initial_datas = await data.json();
@@ -48,33 +49,31 @@ async function getAnime(find) {
         displayAnime(initial_datas.results);
     }
     catch (err) {
-        document.querySelector(".image-conatiner").append("Details Cannot be  Founded");
+        document.querySelector(".form").append("Sorry for the inconvenience, TRY AGAIN LATER");
+        // document.body.append("Details Cannot be  Founded");
+        console.log(err);
+
     }
 }
 
 //-------------------------- displayAnime  ---------------------------
-function displayAnime(anime) {
-    for (let i = 0; i <= anime.length; i++) {
-        //anime card
+function displayAnime(animes) {
+    animes.forEach((anime) => {
         var image_box = document.createElement("div");
         image_box.innerHTML = `
-        <div class="card">
-                <div class="image">
-                  <img src=${anime[i].image_url} alt="image">
-                </div>
-                <div class="content">
-                  <h2>${anime[i].title}</h2>
-                  <p><strong>Start Date:</strong>    ${new Date (anime[i].start_date).toDateString()}</p>
-                  <p><strong>End Date:</strong>      ${new Date (anime[i].end_date).toDateString()}</p>
-                  <p><strong>Type:</strong>          ${anime[i].type}</p>    
-                  <p><strong>Rating:</strong> <i class="fas fa-star"></i> ${anime[i].score}</p> 
-                </div>
-       </div>
-
-`;
+<div class="card">
+        <div class="image">
+          <img src=${anime.image_url} alt="image">
+        </div>
+        <div class="content">
+          <h2>${anime.title}</h2>
+          <p><strong>Start Date:</strong>    ${new Date(anime.start_date).toDateString()}</p>
+          <p><strong>End Date:</strong>      ${new Date(anime.end_date).toDateString()}</p>
+          <p><strong>Type:</strong>          ${anime.type}</p>    
+          <p><strong>Rating:</strong> <i class="fas fa-star"></i> ${anime.score}</p> 
+        </div>
+</div>`;
         document.querySelector(".image-conatiner").append(image_box);
-    }
-   
 
+    });
 }
-
